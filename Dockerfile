6be1dfcc3003522cc68843ce35058d832f1ea04b
Mark Lacasse <mark.lacasse@contrastsecurity.com>
2020-02-06 16:23:17 -0500

    intial commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:alpine as builder 
+
+WORKDIR '/app'
+COPY package.json .
+RUN npm install
+COPY . . 
+RUN npm run build
+
+#second phase that copyies files built in the first phase to run with nginx
+FROM nginx
+COPY --from=builder /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is for Foo, not for you dude!
         </p>
         <a
           className="App-link"
